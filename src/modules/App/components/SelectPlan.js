13923e7d750f611989b052cf2325efe447708124
alicetong <s.sarratee@gmail.com>
2018-07-29 23:32:59 +1000

    -

--- src/modules/App/components/SelectPlan.js
+++ src/modules/App/components/SelectPlan.js
@@ -15,13 +15,13 @@ class SelectPlan extends React.Component {
       <div>
         <AdvertiseText>Online MBA in 1 Year  — Online MBA International Business in 1 Year. Get more information!</AdvertiseText>
         <PanelArea>
-          <MainPanal>
+          <MainPanel>
             <div style={{ width: '100%' }}>
-              <PanalHeaderText style={{ padding: '10px 0px 7px 18px' }}>Become a Premium Member</PanalHeaderText>
+              <PanelHeaderText style={{ padding: '0px 0px 7px 18px' }}>Become a Premium Member</PanelHeaderText>
               <hr />
-              <section style={{ padding: '5px 27px 33px 29px' }}>
-                <PanalHeaderText>Membership type</PanalHeaderText>
-                <PanalSubHeaderText>Explore and choose a plan that best helps your creative journey</PanalSubHeaderText>
+              <section style={{ padding: '0px 27px 33px 29px' }}>
+                <PanelHeaderText>Membership type</PanelHeaderText>
+                <PanelSubHeaderText>Explore and choose a plan that best helps your creative journey</PanelSubHeaderText>
                 <Planarea>
                   <PlanForm>
                     <FormChoice>
@@ -71,43 +71,43 @@ class SelectPlan extends React.Component {
               <hr />
               <section style={{ padding: '0px 25px 20px 25px' }}>
                 <PlanSelectedTopic>Key Highlights</PlanSelectedTopic>
-                <div style={{display:'inline-flex'}}>
+                <div style={{ display: 'inline-flex' }}>
                   <PlanSelected>
-                    <PlanSelectedIcon src={IconA1}/>
+                    <PlanSelectedIcon src={IconA1} />
                     <PlanSelectedHeader>Corporate Dashboard</PlanSelectedHeader>
                     <PlanSelectedDetail>Receive data based on our analyses and comparison of your profile traffic, applicants</PlanSelectedDetail>
                   </PlanSelected>
                   <PlanSelected>
-                    <PlanSelectedIcon src={IconA2}/>
+                    <PlanSelectedIcon src={IconA2} />
                     <PlanSelectedHeader>Global Recommendations</PlanSelectedHeader>
                     <PlanSelectedDetail>Receive personalized recommendations on <b>partnerships and 50 international talents weekly</b></PlanSelectedDetail>
                   </PlanSelected>
                   <PlanSelected>
-                    <PlanSelectedIcon src={IconA3}/>
+                    <PlanSelectedIcon src={IconA3} />
                     <PlanSelectedHeader>Out-of-network Messaging</PlanSelectedHeader>
                     <PlanSelectedDetail>Message users outside your connections; <b>40 users per week</b></PlanSelectedDetail>
                   </PlanSelected>
                   <PlanSelected>
-                    <PlanSelectedIcon src={IconA41}/>
-                    <PlanSelectedIcon src={IconA42}/>
+                    <PlanSelectedIcon src={IconA41} />
+                    <PlanSelectedIcon src={IconA42} />
                     <PlanSelectedHeader>Opportunity Posts</PlanSelectedHeader>
                     <PlanSelectedDetail>Post opportunities and projects for recruitment or collaboration;<b> 5 posts simultaneously</b></PlanSelectedDetail>
                   </PlanSelected>
                 </div>
               </section>
             </div>
-          </MainPanal>
-          <SidePanal>
+          </MainPanel>
+          <SidePanel>
             <div style={{ width: '100%', position: 'relative' }}>
-              <PanalHeaderText style={{ padding: '10px 0px 7px 18px' }}>Request Summary</PanalHeaderText>
+              <PanelHeaderText style={{ padding: '0px 0px 7px 18px' }}>Request Summary</PanelHeaderText>
               <div>
                 <hr />
                 <section style={{ padding: '5px 27px 33px 29px' }}>
-                  <PanalSubHeaderText>Plan Type</PanalSubHeaderText>
-                  <PanalSubHeaderText>Duration</PanalSubHeaderText>
-                  <PanalSubHeaderText>Payment Information</PanalSubHeaderText>
-                  <PanalSubHeaderText>Add Quota</PanalSubHeaderText>
-                  <PanalSubHeaderText>yes</PanalSubHeaderText>
+                  <PanelSubHeaderText>Plan Type</PanelSubHeaderText>
+                  <PanelSubHeaderText>Duration</PanelSubHeaderText>
+                  <PanelSubHeaderText>Payment Information</PanelSubHeaderText>
+                  <PanelSubHeaderText>Add Quota</PanelSubHeaderText>
+                  <PanelSubHeaderText>yes</PanelSubHeaderText>
                 </section>
               </div>
               <div style={{ position: 'absolute', bottom: '0', width: '100%' }}>
@@ -118,7 +118,68 @@ class SelectPlan extends React.Component {
                 </section>
               </div>
             </div>
-          </SidePanal>
+          </SidePanel>
+          <PaymentMethod>
+            <section style={{ padding: '5px 27px 33px 29px' }}>
+              <PanelHeaderText>Payment method</PanelHeaderText>
+              <PanelSubHeaderText>Subscribe monthly or annually, but fees will always be charged monthly</PanelSubHeaderText>
+              <PayChoices>
+                <div style={{ width: '50%' }}>
+                  <ChoiceBox><input type="radio" name="paymentmethod" value="optionA" />
+                    Subscribe monthly <br />
+                  </ChoiceBox>
+                  <br /><br />
+                  <PriceBox >
+                    <h2>HKD 999</h2>
+                    <br />
+                    <PriceCondition>Charged every month, for minimum 3-months' purchase</PriceCondition>
+                  </PriceBox >
+                </div>
+                <div style={{ width: '50%' }}>
+                  <ChoiceBox><input type="radio" name="paymentmethod" value="optionB" />
+                    Subscribe annually<br />
+                  </ChoiceBox>
+                  <PaymentMText>Discount offered for annual subscription!</PaymentMText>
+                  <PriceBox >
+                    <h2>HKD 799</h2>
+                    <br />
+                    <PriceCondition>Charged every month, for 1-year purchase</PriceCondition>
+                  </PriceBox>
+                </div>
+              </PayChoices>
+            </section>
+          </PaymentMethod>
+          <SubsBox>
+            <section style={{ padding: '5px 27px 33px 29px' }}>
+              <PanelHeaderText>Subscription duration</PanelHeaderText>
+              <PanelSubHeaderText>Indicate how long you want to be a member to this plan</PanelSubHeaderText>
+              <p>Please select number of months you want to subscribe.</p>
+              <SelectMonth>
+                <option value="3">3</option>
+              </SelectMonth>
+              months
+            </section>
+          </SubsBox>
+          <QuotaBox>
+            <section style={{ padding: '5px 27px 33px 29px' }}>
+              <PanelHeaderText>Added quota</PanelHeaderText>
+              <PanelSubHeaderText>Select to add quota to the below features</PanelSubHeaderText>
+              <PayChoices>
+                <div >
+                  <ChoiceBox><input type="radio" name="quota" value="true" />
+                    Add quota <br />
+                  </ChoiceBox>
+                  <br />
+                  <div>
+                    <p>Out-of-network Messaging  <b>+3 per week</b></p>
+                    <p>Opportunity Post  <b>+1</b></p>
+                    <p style={{color: '#8c8c8c'}}>HKD 30 per month</p>
+                    <PaymentMText>Learn More</PaymentMText>
+                  </div >
+                </div>
+              </PayChoices>
+            </section>
+          </QuotaBox>
         </PanelArea>
       </div>
     );
@@ -140,7 +201,7 @@ grid-gap: 1em;
 margin: 0 auto;
 padding: 0 10% 2% 10%;
 `;
-const MainPanal = styled.div`
+const MainPanel = styled.div`
 grid-column: 1 / span 3;
 display:flex;
 background-color: #ffffff;
@@ -149,7 +210,7 @@ box-shadow: 0px 1px 2px rgba(0,0,0,0.4);
   grid-column: 1 / span 5;
 }
 `;
-const SidePanal = styled.div`
+const SidePanel = styled.div`
 grid-column: span 1;
 display:flex;
 background-color: #ffffff;
@@ -158,13 +219,41 @@ box-shadow: 0px 1px 2px rgba(0,0,0,0.4);
   grid-column: 1 / span 5;
 }
 `;
-const PanalHeaderText = styled.h3`
+
+const PaymentMethod = styled.div`
+grid-column: 1 / span 3;
+display:flex;
+background-color: #ffffff;
+box-shadow: 0px 1px 2px rgba(0,0,0,0.4);
+@media (max-width: 700px) {
+  grid-column: 1 / span 5;
+}
+`;
+const SubsBox = styled.div`
+grid-column: 1 / span 3;
+display:flex;
+background-color: #ffffff;
+box-shadow: 0px 1px 2px rgba(0,0,0,0.4);
+@media (max-width: 700px) {
+  grid-column: 1 / span 5;
+}
+`;
+const QuotaBox = styled.div`
+grid-column: 1 / span 3;
+display:flex;
+background-color: #ffffff;
+box-shadow: 0px 1px 2px rgba(0,0,0,0.4);
+@media (max-width: 700px) {
+  grid-column: 1 / span 5;
+}
+`;
+const PanelHeaderText = styled.h3`
 font-size: 16px;
 color: #303030;
 font-weight: 700;
-margin: 5px 0px;
+margin: 26px 0px 5px 0px;
 `;
-const PanalSubHeaderText = styled.h4`
+const PanelSubHeaderText = styled.h4`
 font-size: 14px;
 color: #8c8c8c;
 font-weight: 300;
@@ -267,4 +356,40 @@ text-decoration: none solid rgb(138, 138, 138);
 line-height: 14px;
 text-align: center;
 
+`;
+const PayChoices = styled.div`
+
+display: inline-flex;
+margin: 38px 0px 0px 0px;
+
+`;
+const PaymentMText = styled.p`
+
+font-size: 14px;
+color: #ffc800;
+text-decoration: none solid rgb(255, 200, 0);
+line-height: 16px;
+margin: 10px 0px 30px 0px;
+`;
+const PriceCondition = styled.h5`
+font-weight: normal;
+font-size: 13px;
+color: #1c1c1c;
+text-decoration: none solid rgb(28, 28, 28);
+line-height: 17px;
+`;
+const PriceBox = styled.div`
+
+text-align: 'left'
+
+`;
+
+const SelectMonth = styled.select`
+width: 53px;
+height: 28px;
+border: 1px solid #d1d1d1;
+font-size: 14px;
+color: #303030;
+text-decoration: none solid rgb(48, 48, 48);
+margin-right: 5px;
 `;
